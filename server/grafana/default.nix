b6c96a0e30fbd4a06ce2362ce0d30075be83020a
Soner Sayakci <s.sayakci@shopware.com>
2023-03-21 15:43:03 +0100

    feat: setup own github runner

--- server/grafana/default.nix
+++ server/grafana/default.nix
@@ -5,15 +5,11 @@
     ./prometheus.nix
     ./postgresql.nix
     ./minio.nix
-    ./gitlab.nix
+    ./github.nix
     ./blackfire.nix
   ];
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-  environment.systemPackages = [
-    pkgs.openssh
-    pkgs.git
-  ];
 }
---
+++ server/grafana/github.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, vars, ... }: {
+  sops.secrets = {
+    githubRunnerToken = {
+      sopsFile = ./secrets/github.yaml;
+    };
+  };
+
+  services.github-runners.default = {
+    enable = true;
+    url = "https://github.com/shopware/shopware-benchmark";
+    tokenFile = config.sops.secrets.githubRunnerToken.path;
+    extraPackages = [
+      pkgs.openssh
+      pkgs.git
+    ];
+    extraEnvironment = {
+      TMP = "/tmp";
+      TMPDIR = "/tmp";
+      ANSIBLE_LOCAL_TEMP = "/tmp";
+    };
+  };
+}
